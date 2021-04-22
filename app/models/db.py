8de2a91680d9424694adf363ab52dc0f85d91912
Jim N <75219511+guny12@users.noreply.github.com>
2021-04-22 19:34:02 -0400

    Update db.py

--- app/models/db.py
+++ app/models/db.py
@@ -5,36 +5,6 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-
-# class User(db.Model):
-#     __tablename__ = 'users'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     firstName = db.Column(db.String(50), nullable=False)
-#     lastName = db.Column(db.String(50), nullable=False)
-#     email = db.Column(db.String(75), unique=True, nullable=False)
-#     username = db.Column(db.String(50), unique=True, nullable=False)
-#     hashedPassword = db.Column(db.String(100), nullable=False)
-#     createdAt = db.Column(db.DateTime, default=datetime.now())
-#     updatedAt = db.Column(db.DateTime, default=datetime.now())
-
-#     # associations
-#     userList = relationship('List', backref='listUser')
-#     userTask = relationship('Task', backref='taskUser')
-#     userNote = relationship('Note', backref='noteUser')
-
-#     @property
-#     def password(self):
-#         return self.hashedPassword
-
-#     @password.setter
-#     def password(self, password):
-#         self.hashedPassword = generate_password_hash(password)
-
-#     def check_password(self, password):
-#         return check_password_hash(self.password, password)
-
-
 class List(db.Model):
     __tablename__ = 'lists'
 
@@ -45,26 +15,27 @@ class List(db.Model):
     updatedAt = db.Column(db.DateTime, default=datetime.now())
 
     # associations
-    listTask = relationship('Task', backref='taskList')
+    listTask = relationship('Task', backref='taskList', cascade="all,delete-orphan")
 
 
 class Task(db.Model):
     __tablename__ = 'tasks'
 
     id = db.Column(db.Integer, primary_key=True)
-    creatorId = db.Column(
-        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    creatorId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     listId = db.Column(db.Integer, db.ForeignKey('lists.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     completed = db.Column(db.Boolean, default=False)
     startDate = db.Column(db.DateTime)
     dueDate = db.Column(db.DateTime)
-    priority = db.Column(db.Integer)
+    priority = db.Column(db.Integer, default=0)
+    # min max
     createdAt = db.Column(db.DateTime, default=datetime.now())
     updatedAt = db.Column(db.DateTime, default=datetime.now())
 
     # associations
-    taskNote = relationship('Note', backref='noteTask')
+    taskNote = relationship('Note', backref='noteTask', cascade="all,delete-orphan")
+    
 
 
 class GiveToUser(db.Model):
@@ -75,8 +46,9 @@ class GiveToUser(db.Model):
     taskId = db.Column(db.Integer, db.ForeignKey('tasks.id'), nullable=False)
 
     # associations
-    giveUser = relationship('User', backref=db.backref('giveUserTask'))
-    giveTask = relationship('Task', backref=db.backref('giveTaskUser'))
+    # TEST THE CASCADE DELETE. remove the cascade if it breaks
+    giveUser = relationship('User', backref=db.backref('giveUserTask'), cascade="all,delete-orphan")
+    giveTask = relationship('Task', backref=db.backref('giveTaskUser'), cascade="all,delete-orphan")
 
 
 class Note(db.Model):
