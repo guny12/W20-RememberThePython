2088f30fe8ff5420a78a696d2897b436403285fa
Jim <Jimjnguy@gmail.com>
2021-04-23 13:30:04 -0400

    starting to create user Store and modify modals

---
+++ react-app/src/components/LoginFormModal/LoginForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
+import "./LoginForm.css";
+import { Button, Form } from "react-bootstrap";
+
+const LoginForm = () => {
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [credential, setCredential] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErrors([]);
+		console.log(credential, password, "CREDENTIAL AND PASSWORD -------------------");
+		return dispatch(sessionActions.login({ credential, password }))
+			.then((response) => (response.ok ? history.push("/home") : response))
+			.catch(async (res) => {
+				const data = await res.json();
+				if (data && data.errors) setErrors(data.errors);
+			});
+	};
+
+	return (
+		<Form onSubmit={handleSubmit} className="loginform__Form">
+			<ul>
+				{errors.map((error, idx) => (
+					<li key={idx}>{error}</li>
+				))}
+			</ul>
+			<Form.Group controlId="formBasicEmail">
+				<Form.Label>Username or Email</Form.Label>
+				<Form.Control
+					type="text"
+					autoComplete="username"
+					value={credential}
+					onChange={(e) => setCredential(e.target.value)}
+					required
+					placeholder="Enter Username or Email"
+				/>
+			</Form.Group>
+			<Form.Group controlId="formGroupPassword">
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					type="password"
+					autoComplete="current-password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					required
+					placeholder="Enter Password"
+				/>
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Log In
+			</Button>
+		</Form>
+	);
+};
+export default LoginForm;
---
+++ react-app/src/components/store/session.js
@@ -0,0 +1,76 @@
+//action type variables
+const SET_SESSION = "session/SET_SESSION";
+const REMOVE_SESSION = "session/REMOVE_SESSION";
+
+// action creators
+const setSessionUser = (user) => ({
+	type: SET_SESSION,
+	user,
+});
+
+const removeSessionUser = () => ({
+	type: REMOVE_SESSION,
+});
+
+// thunk action creators
+export const login = (user) => async (dispatch) => {
+	const { credential, password } = user;
+	const response = await fetch("/api/session", {
+		method: "POST",
+		body: JSON.stringify({ credential, password }),
+	});
+	const data = await response.json();
+	dispatch(setSessionUser(data.user));
+	return response;
+};
+
+export const restoreUser = () => async (dispatch) => {
+	const response = await fetch("/api/session");
+	const data = await response.json();
+	dispatch(setSessionUser(data.user));
+	return response;
+};
+
+export const signUp = (user) => async (dispatch) => {
+	const { username, email, password, firstName, lastName } = user;
+	const response = await fetch("/api/users", {
+		method: "POST",
+		body: JSON.stringify({
+			username,
+			email,
+			password,
+			firstName,
+			lastName,
+		}),
+	});
+	const data = await response.json();
+	dispatch(setSessionUser(data.user));
+	return response;
+};
+
+export const logout = () => async (dispatch) => {
+	const response = await fetch("/api/session", { method: "DELETE" });
+	dispatch(removeSessionUser());
+	return response;
+};
+
+// reducer
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+	let newState;
+	switch (action.type) {
+		case SET_SESSION:
+			newState = Object.assign({}, state);
+			newState.user = action.user ? action.user : null;
+			return newState;
+		case REMOVE_SESSION:
+			newState = Object.assign({}, state);
+			newState.user = null;
+			return newState;
+		default:
+			return state;
+	}
+};
+
+export default sessionReducer;
