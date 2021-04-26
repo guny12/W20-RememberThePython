970822c066b1d299947c2bd029f20cb18c5bb0b8
Jim <Jimjnguy@gmail.com>
2021-04-26 19:28:20 -0400

    added AllTasks Component, to render in side nav. this will be a place holder until Tasks component actually gets created and put inside to pass each task

--- react-app/src/components/SideNavigation/allTasks.js
+++ react-app/src/components/SideNavigation/allTasks.js
@@ -4,46 +4,34 @@ import { useHistory } from "react-router-dom";
 import * as taskActions from "../../store/tasks";
 
 const AllTasks = () => {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const tasks = useSelector((state) => state.tasks.allTasks);
-  console.log(tasks, "------------------------TASKS")
-  // const handleDelete = async (e) => {
-  //   e.preventDefault();
-  //   const toBeDeleted = {
-  //     listId: e.target.id,
-  //   };
-  //   await dispatch(deleteList(toBeDeleted));
-  //   dispatch(getAllLists());
-  //   return history.push("/lists");
-  // };
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const tasks = useSelector((state) => state.tasks.allTasks);
+	console.log(tasks, "------------------------TASKS");
+	// const handleDelete = async (e) => {
+	//   e.preventDefault();
+	//   const toBeDeleted = {
+	//     listId: e.target.id,
+	//   };
+	//   await dispatch(deleteList(toBeDeleted));
+	//   dispatch(getAllLists());
+	//   return history.push("/lists");
+	// };
 
-  useEffect(() => {
-    dispatch(taskActions.getTasks());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(taskActions.getTasks());
+	}, [dispatch]);
 
-  let taskList = (
-    Object.values(tasks)
-  )
-  return (
-    <div>
-      <div>
-        <h2>Tasks</h2>
-      </div>
-      <div>
-        {for (task in tasks) {
-          return (
-            <div key={task.id}>
-            {task.id}
-            {/* <button id={lis.id} onClick={handleDelete}>
-              DELETE
-            </button> */}
-          </div>
-          )
-        }}
-      </div>
-    </div>
-  );
+	let tasksDiv = Object.values(tasks);
+
+	return (
+		<div>
+			{tasksDiv?.map((task) => (
+				// <Task task={task}/>
+				<div key={task.id}>{task.id}</div>
+			))}
+		</div>
+	);
 };
 
 export default AllTasks;
