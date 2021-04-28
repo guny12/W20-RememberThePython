8fee2cdb272d5e9afde7daff60590488034b619e
Jim <Jimjnguy@gmail.com>
2021-04-28 02:56:54 -0400

    removed case for non logged in users in /Home because it's a protected route again

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -13,13 +13,6 @@ const Home = () => {
 		})();
 	}, [dispatch]);
 
-	if (!currentUser) {
-		return (
-			<h1>
-				Welcome to MemberBerry, please sign up or log in to get started<p> you can also test it out as a demo user</p>
-			</h1>
-		);
-	}
 	return <></>;
 };
 
