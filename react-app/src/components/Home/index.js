904866b4a0ac5a34174c4f6bc62e9519fcebb335
Jim <Jimjnguy@gmail.com>
2021-04-29 18:16:47 -0400

    removed console.logs

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Home.css";
 
 const Home = ({ listLoaded }) => {
-	console.log("WAS SENT TO HOME------------------------------------");
 	if (listLoaded) document.querySelector("#sideNav-tab-home").click();
 
 	return <></>;
