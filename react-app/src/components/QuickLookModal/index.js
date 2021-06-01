115cc4b8b037e8cf1da2d869d0c985e7f4060a89
josh <joshtupas@gmail.com>
2021-06-01 13:27:42 -0500

    added tooltip for quick look cards, hovering over 'Uncompleted' displays a tooltip of 'Click to see all tasks'

--- react-app/src/components/QuickLookModal/index.js
+++ react-app/src/components/QuickLookModal/index.js
@@ -2,16 +2,23 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import QuickLook from "./QuickLook";
-import { Button } from "react-bootstrap";
+import renderTooltip from "./tooltip";
+import { Button, OverlayTrigger } from "react-bootstrap";
 
 function QuickLookModal({ listId }) {
 	const [showModal, setShowModal] = useState(false);
 
 	return (
 		<>
-			<Button id="QuickLookButton" className="card-footer-Quicklook" variant="dark" onClick={() => setShowModal(true)}>
-				Uncompleted
-			</Button>
+			<OverlayTrigger
+				placement="auto"
+				delay={{ show: 250, hide: 50 }}
+				overlay={renderTooltip}
+			>
+				<Button id="QuickLookButton" className="card-footer-Quicklook" variant="dark" onClick={() => setShowModal(true)}>
+					Uncompleted
+				</Button>
+			</OverlayTrigger>
 			{showModal && (
 				<Modal onClose={() => setShowModal(false)}>
 					<QuickLook listId={listId} />
